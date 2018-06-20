1d5b8b7aeb0bba22aa3ef4b7b145ea6356f8d436
gilpanal <gilpanal@gmail.com>
2018-06-20 23:52:35 +0200

    First commit
    A new concept for collaborative music

---
+++ dist/bunchofsongs/js/web-audio-editor.js
@@ -0,0 +1,74 @@
+var playlist = WaveformPlaylist.init({
+  samplesPerPixel: 3000,
+  waveHeight: 100,
+  container: document.getElementById("playlist"),
+  state: 'cursor',
+  colors: {
+    waveOutlineColor: '#E0EFF1',
+    timeColor: 'grey',
+    fadeColor: 'black'
+  },
+  timescale: true,
+  controls: {
+    show: true, //whether or not to include the track controls
+    width: 200 //width of controls in pixels
+  },
+  seekStyle : 'line',
+  zoomLevels: [500, 1000, 3000, 5000]
+});
+
+playlist.load([
+  {
+    "src": "media/Estefa/Acustica_00.wav",
+    "name": "Guitar",
+    "fadeIn": {
+      "duration": 0.5
+    },
+    "fadeOut": {
+      "duration": 0.5
+    },    
+    
+    "waveOutlineColor": '#c0dce0'
+  },
+  {
+    "src": "media/Estefa/Acustica_01.wav",
+    "name": "Guitar",
+    "start": 8.5,
+    "gain": 0.10,
+    "fadeIn": {
+      "shape": "logarithmic",
+      "duration": 0.5
+    },
+    "fadeOut": {
+      "shape": "logarithmic",
+      "duration": 0.5
+    }
+  },
+  {
+    "src": "media/Estefa/Voz_01.wav",
+    "name": "Vocals",
+    "start": 23.5,
+    "customClass": "vocals",
+    "fadeOut": {
+      "shape": "linear",
+      "duration": 0.5
+    },
+    "cuein": 15
+  },
+  {
+    "src": "media/Estefa/Voz_02.wav",
+    "name": "Vocals",
+    "start": 23.5,
+    "customClass": "vocals",
+    "fadeOut": {
+      "shape": "linear",
+      "duration": 0.5
+    },
+    "cuein": 15
+  }
+]).then(function() {
+  //can do stuff with the playlist.
+
+  //initialize the WAV exporter.
+  playlist.initExporter();
+});
