eaeb668df2048f0317c70ccdd5a694f9f06479b5
josemanuel <g.josemanuel@ext.douglas.eu>
2020-05-28 13:40:46 +0200

    Firebase multiple hosting

--- src/js/config_template.js
+++ src/js/config_template.js
@@ -1,24 +1,17 @@
 const MODE = 'DEV'
 
-// TELEGRAM BOT INFO: TOKEN, NAME
-// More info: https://core.telegram.org/bots#6-botfather
-
 // PROD
 
-const BOT_TOKEN_PROD = ''
 const BOT_NAME_PROD = ''
 const BASE_ENDPOINT_PROD = ''
 const UPLOAD_ENDPOINT_PROD = ''
 
 // DEV
   
-const BOT_TOKEN_DEV = ''
 const BOT_NAME_DEV = ''
 const BASE_ENDPOINT_DEV = ''
 const UPLOAD_ENDPOINT_DEV = ''
 
-
-export const BOT_TOKEN = (MODE === 'PROD') ? BOT_TOKEN_PROD : BOT_TOKEN_DEV
 
 export const BOT_NAME = (MODE === 'PROD') ? BOT_NAME_PROD : BOT_NAME_DEV
 
